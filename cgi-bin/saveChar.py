5e447b23c54c3461ea4d3550a6ce45aa827a8116
nyix <rsa17826@email.vccs.edu>
2026-07-09 13:20:57 -0400

    fixed base save file

--- cgi-bin/saveChar.py
+++ cgi-bin/saveChar.py
@@ -9,7 +9,7 @@ reg = re.compile("&stat\\d+=(-?\\d+(?:\\.\\d+)?)")
 try:
   query_string = os.environ.get("QUERY_STRING", "")
   parsed_params = parse_qs(query_string)
-  saveFile = parsed_params.get("saveFile", ["nonAp"])[0]
+  saveFile = parsed_params.get("saveFile", ["nonAP"])[0]
   with open(
     os.path.join(os.path.dirname(__file__), f"../MQFiles/loadChar_{saveFile}.php"),
     "w",
