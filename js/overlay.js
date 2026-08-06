58864c100362bc47c9d77e4852eba5953fd169bf
nyix <rsa17826@email.vccs.edu>
2026-08-06 09:38:48 -0400

    NO MESSAGE SET

--- js/overlay.js
+++ js/overlay.js
@@ -507,11 +507,11 @@ function customDrawLoop() {
   for (let hint of HintTracker.all) {
     if (hint.found) continue
     // TODO get game, could be an out hint not always in
-    var itemName = ap.itemIdToName["MathQuest"][hint.item]
     var finder = ap.players.find((e) => e.slot == hint.finding_player)
     var owner = ap.players.find(
       (e) => e.slot == hint.receiving_player,
     )
+    var itemName = ap.itemIdToName[ap.slotInfo[hint.receiving_player].game][hint.item]
     var finderName =
       hint.finding_player == ap.slot ? "your"
       : finder.alias == finder.name ? `${finder.name}'s`
